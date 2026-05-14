ec28c7953890e492effa3d6ab8be2497f725022f
Carlos Castañeda <carloscasta.dev@gmail.com>
2026-05-14 16:27:59 +0200

    correcciones

--- scripts/setup_arena_part02.sh
+++ scripts/setup_arena_part02.sh
@@ -10,17 +10,17 @@ cp -rf ./scripts/data/compose.yaml ./compose.yaml
 ./vendor/bin/sail composer require laravel/sanctum
 ./vendor/bin/sail artisan vendor:publish --provider="Laravel\Sanctum\SanctumServiceProvider"
 cp -rf ../scripts/data/app/Models/User.php ./app/Models/User.php
-cp -rf ../scripts/data/app/config/cors.php ./app/config/cors.php
+cp -rf ../scripts/data/config/cors.php ./config/cors.php
 cp -rf ../scripts/data/.env ./.env
 ./vendor/bin/sail artisan config:clear
 cp -rf ../scripts/data/bootstrap/app.php ./bootstrap/app.php
 cp -rf ../scripts/data/routes/api.php ./routes/api.php
 ./vendor/bin/sail artisan make:controller Auth/AuthController
-cp -rf ../scripts/data/app/Http/Controllers/AuthController.php ./app/Http/Controllers/AuthController.php
+cp -rf ../scripts/data/app/Http/Controllers/Auth/AuthController.php ./app/Http/Controllers/Auth/AuthController.php
 ./vendor/bin/sail artisan make:middleware IsAdmin
-cp -rf ../app/Http/Middleware/IsAdmin.php/scripts ./app/Http/Middleware/IsAdmin.php
+cp -rf ../scripts/data/app/Http/Middleware/IsAdmin.php ./app/Http/Middleware/IsAdmin.php
 cp -rf ../scripts/data/bootstrap/appv2.php ./bootstrap/app.php
-sail artisan migrate
+./vendor/bin/sail artisan migrate
 
 echo "alias sail='./vendor/bin/sail'" >> ~/.bashrc
 
