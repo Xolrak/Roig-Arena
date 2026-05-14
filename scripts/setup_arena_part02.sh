a2ebf7e03d22551cadb06f877d2e0ecc39b93800
Carlos Castañeda <carloscasta.dev@gmail.com>
2026-05-14 12:55:21 +0200

    Anadido tests

--- scripts/setup_arena_part02.sh
+++ scripts/setup_arena_part02.sh
@@ -2,6 +2,14 @@
 
 curl -s "https://laravel.build/roig-arena?with=mysql,redis,meilisearch,mailpit,selenium" | bash
 
+cp -rf ./scripts/roig-arena.data/* ./roig-arena/
+rm -rf ./scripts/roig-arena.data
+
+cd roig-arena
+./vendor/bin/sail up -d
+./vendor/bin/sail composer require laravel/sanctum
+sail artisan migrate:fresh --seed
+
 echo "alias sail='./vendor/bin/sail'" >> ~/.bashrc
 
 echo "Ejecuta source ~/.bashrc para poder usar el alias de sail"
