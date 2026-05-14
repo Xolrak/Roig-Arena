0f1458e6e711563d8242422c2593724a6cd74487
Carlos Castañeda <carloscasta.dev@gmail.com>
2026-05-15 00:14:58 +0200

    Tests añadidos

--- scripts/setup_tests.sh
+++ scripts/setup_tests.sh
@@ -16,3 +16,15 @@ cp -rf ../scripts/data/database/factories/EventoFactory.php ./database/factories
 cp -rf ../scripts/data/database/factories/PrecioFactory.php ./database/factories/PrecioFactory.php
 cp -rf ../scripts/data/database/factories/EstadoAsientoFactory.php ./database/factories/EstadoAsientoFactory.php
 cp -rf ../scripts/data/database/factories/EntradaFactory.php ./database/factories/EntradaFactory.php
+
+## TESTS
+# features
+cp -rf ../scripts/data/tests/Feature/AuthTest.php ./tests/Feature/AuthTest.php
+cp -rf ../scripts/data/tests/Feature/EventoTest.php ./tests/Feature/EventoTest.php
+cp -rf ../scripts/data/tests/Feature/ReservaTest.php ./tests/Feature/ReservaTest.php
+cp -rf ../scripts/data/tests/Feature/CompraTest.php ./tests/Feature/CompraTest.php
+# unit
+cp -rf ../scripts/data/tests/Unit/ModeloTest.php ./tests/Unit/ModeloTest.php
+cp -rf ../scripts/data/tests/Unit/ReservaServiceTest.php ./tests/Unit/ReservaServiceTest.php
+cp -rf ../scripts/data/tests/Unit/CompraServiceTest.php ./tests/Unit/CompraServiceTest.php
+cp -rf ../scripts/data/tests/Unit/LiberarReservasServiceTest.php ./tests/Unit/LiberarReservasServiceTest.php
