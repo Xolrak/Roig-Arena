a0ed9e046d97d9f5c8ed3cb36741f6d9d2fe99d4
Carlos Castañeda <carloscasta.dev@gmail.com>
2026-05-14 14:54:51 +0200

    Reset

--- scripts/setup_arena_part02.sh
+++ scripts/setup_arena_part02.sh
@@ -2,13 +2,8 @@
 
 curl -s "https://laravel.build/roig-arena?with=mysql,redis,meilisearch,mailpit,selenium" | bash
 
-cp -rf ./scripts/roig-arena.data/* ./roig-arena/
-rm -rf ./scripts/roig-arena.data
-
 cd roig-arena
 ./vendor/bin/sail up -d
-./vendor/bin/sail composer require laravel/sanctum
-sail artisan migrate:fresh --seed
 
 echo "alias sail='./vendor/bin/sail'" >> ~/.bashrc
 
